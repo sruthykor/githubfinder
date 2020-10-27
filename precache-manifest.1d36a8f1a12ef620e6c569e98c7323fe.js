a1670e944589257a93161c72e8745c9544751a5e
sruthykor <sruthykor@gmail.com>
2020-10-27 23:21:55 +1100

    Updates

---
+++ precache-manifest.1d36a8f1a12ef620e6c569e98c7323fe.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ff22ff906d88c161ffb34d4e6d0bc3f9",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d90085333b9c35a1f38a",
+    "url": "/static/css/2.36020323.chunk.css"
+  },
+  {
+    "revision": "d90085333b9c35a1f38a",
+    "url": "/static/js/2.11df3d5a.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.11df3d5a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "64ff81d9f80fe8470381",
+    "url": "/static/js/main.75b4d385.chunk.js"
+  },
+  {
+    "revision": "7dbaecb9b1566e9ab509",
+    "url": "/static/js/runtime-main.9847c3b2.js"
+  }
+]);
